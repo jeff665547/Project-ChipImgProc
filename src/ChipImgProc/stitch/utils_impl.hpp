9fd7466da8e7d5934e7473b7b2b7135ef463708d
Chia Hua Chang <johnidfet@gmail.com>
2018-06-12 17:38:37 +0800

    remove debug message

--- src/ChipImgProc/stitch/utils_impl.hpp
+++ src/ChipImgProc/stitch/utils_impl.hpp
@@ -53,30 +53,25 @@ cv::Mat add(
 ){
     auto res_roi = get_full_w_h(imgs, st_ps);
     cv::Mat res(res_roi.height, res_roi.width, imgs.front().type());
-    std::cout << __FILE__ << ":" << __LINE__ << std::endl;
     auto norm_st_ps = normalize_points(
         cv::Point_<int>(res_roi.x, res_roi.y),
         st_ps
     );
-    std::cout << __FILE__ << ":" << __LINE__ << std::endl;
     std::vector<cv::Rect> added_rois;
     for( int i = 0; i < imgs.size(); i ++ ) {
         auto& img_i = imgs.at(i);
         auto norm_ps(norm_st_ps.at(i));
-        std::cout << __FILE__ << ":" << __LINE__ << std::endl;
         cv::Rect roi(
             norm_ps.x, norm_ps.y,
             img_i.cols, img_i.rows
         );
         img_i.copyTo(res(roi));
         if( !added_rois.empty() ) {
-            std::cout << __FILE__ << ":" << __LINE__ << std::endl;
             for( int j = 0; j < added_rois.size(); j ++) {
                 auto& aroi = added_rois.at(j);
                 auto& img_j = imgs.at(j);
                 cv::Rect inter(roi & aroi);
                 cv::Mat overlap;
-                std::cout << __FILE__ << ":" << __LINE__ << std::endl;
                 std::cout << "roi: " << roi <<std::endl;
                 std::cout << "aroi: " << aroi <<std::endl;
                 std::cout << "inter: " << inter <<std::endl;
@@ -99,9 +94,7 @@ cv::Mat add(
                         0,
                         overlap
                     );
-                    std::cout << __FILE__ << ":" << __LINE__ << std::endl;
                     overlap.copyTo(res(inter));
-                    std::cout << __FILE__ << ":" << __LINE__ << std::endl;
                 }
             }
         }
