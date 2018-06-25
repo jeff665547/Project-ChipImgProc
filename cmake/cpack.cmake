9cc5fb974588bca90d7c1c2ca4ef9647350d7ff4
ChiaHuaChang <johnidfet@gmail.com>
2018-06-25 20:33:13 +0800

    fix cpack bug

--- cmake/cpack.cmake
+++ cmake/cpack.cmake
@@ -5,7 +5,7 @@ if( ENABLE_CPACK )
     set(CPACK_PACKAGE_NAME ${PROJECT_NAME})
     set(CPACK_PACKAGE_CONTACT "cpack.contact")
     set(CPACK_PACKAGE_VENDOR  "Centrillion.TW")
-    set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "Summit - Microarray Reader Software")
+    set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "Centrillion chip image process SDK")
     set(CPACK_PACKAGE_VERSION ${${PROJECT_NAME}_VERSION})
     set(CPACK_PACKAGE_FILE_NAME "${PROJECT_NAME}-v${${PROJECT_NAME}_VERSION}")
     set(CPACK_COMPONENTS_ALL
@@ -27,7 +27,7 @@ if( ENABLE_CPACK )
         set( CPACK_NSIS_CONTACT "cpack.nsis.contact")
         set (CPACK_NSIS_COMPRESSOR "/SOLID lzma")
     endif()
-    include(CPACK)
+    include(CPack)
     # cpack_add_component_group( Development
     #     DISPLAY_NAME "Summit software development kit"
     #     EXPENDED)
