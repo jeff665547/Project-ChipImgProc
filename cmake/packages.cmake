11aab70760a6115079d2ee2454c9af7414aa57f7
ChiaHuaChang <johnidfet@gmail.com>
2018-06-25 21:03:22 +0800

    update package config opencv use shared libs

--- cmake/packages.cmake
+++ cmake/packages.cmake
@@ -21,13 +21,13 @@ if( MINGW )
         OpenCV
         VERSION ${HUNTER_OpenCV_VERSION}
         CMAKE_ARGS 
-            BUILD_SHARED_LIBS=ON
+            BUILD_SHARED_LIBS=OFF
             ENABLE_PRECOMPILED_HEADERS=OFF
     )
 else()
     hunter_config(
         OpenCV
         VERSION ${HUNTER_OpenCV_VERSION}
-        CMAKE_ARGS BUILD_SHARED_LIBS=ON
+        CMAKE_ARGS BUILD_SHARED_LIBS=OFF
     )
 endif()
