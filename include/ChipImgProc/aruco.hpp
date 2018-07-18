5c35d71d2d23b34cde2a556fa140ecf621e47ea8
ChiaHuaChang <johnidfet@gmail.com>
2018-07-18 16:24:11 +0800

    update testing

--- include/ChipImgProc/aruco.hpp
+++ include/ChipImgProc/aruco.hpp
@@ -2,19 +2,23 @@
 #include <vector>
 #include <numeric>
 #include <nlohmann/json.hpp>
+#include <bitset>
 namespace chipimgproc{ 
 
 struct ArUco {
     static int bin_to_dec( const std::vector<bool>& bin_vec ) {
-        return std::accumulate(bin_vec.rbegin(), bin_vec.rend(), 0, [](int x, int y) {
+        return std::accumulate(bin_vec.rbegin(), bin_vec.rend(), 0, [](std::int64_t x, std::int64_t y) {
             return (x << 1) + y;
         });
     }
-    static int bin_to_dec( const nlohmann::json& bin_json_arr ) {
-        return std::accumulate(bin_json_arr.rbegin(), bin_json_arr.rend(), 0, [](const auto& a, const auto& b){
-            int ia = a;
-            int ib = b;
-            return (ia << 1) + ib;
+    static std::int64_t bin_to_dec( const nlohmann::json& bin_json_arr ) {
+        std::int64_t n = 0;
+        return std::accumulate(bin_json_arr.begin(), bin_json_arr.end(), n, [](const auto& a, const auto& b){
+            std::int64_t ia = a;
+            std::int64_t ib = b;
+            std::int64_t tmp = ia << 1ULL;
+            tmp += ib;
+            return tmp;
         });
     }
 };
--- unit_test/ChipImgProc/aruco_test.cpp
+++ unit_test/ChipImgProc/aruco_test.cpp
@@ -4,11 +4,12 @@
 #include <nlohmann/json.hpp>
 #include <fstream>
 TEST(aruco, bin_to_int) {
-    auto json_path = nucleona::test::data_dir() / "aruco_marker_layout.json";
+    auto json_path = nucleona::test::data_dir() / "aruco_default_dict_6x6_250.json";
     std::ifstream fin(json_path.string());
     nlohmann::json mk_layout;
     fin >> mk_layout;
     for( auto&& mk : mk_layout["marker_list"] ) {
+        // std::cout << mk << std::endl;
         std::cout << chipimgproc::ArUco::bin_to_dec(mk) << std::endl;
     }
 }
