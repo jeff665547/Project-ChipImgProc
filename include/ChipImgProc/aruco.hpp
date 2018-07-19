887c0f9aa02f9bd20dd9b6b335a03b1ce34b91d5
Chia Hua Chang <johnidfet@gmail.com>
2018-07-19 10:37:58 +0800

    update aruco algorithm

--- include/ChipImgProc/aruco.hpp
+++ include/ChipImgProc/aruco.hpp
@@ -3,6 +3,8 @@
 #include <numeric>
 #include <nlohmann/json.hpp>
 #include <bitset>
+#include <ChipImgProc/utils.h>
+#include <Nucleona/stream/null_buffer.hpp>
 namespace chipimgproc{ 
 
 struct ArUco {
@@ -21,6 +23,31 @@ struct ArUco {
             return tmp;
         });
     }
+    static std::int64_t mat_to_dec( 
+        const cv::Mat& m, 
+        std::ostream& logger = nucleona::stream::null_out
+    ) {
+        cv::Mat_<std::uint8_t> tmp = m;
+        std::int64_t n = 0;
+        return std::accumulate(tmp.begin(), tmp.end(), n, [](const auto& a, const auto& b){
+            std::int64_t ia = a;
+            std::int64_t ib = b;
+            std::int64_t tmp = ia << 1ULL;
+            tmp += ib;
+            return tmp;
+        });
+    }
+    static cv::Mat dec_to_mat(std::int64_t n, int row, int col) {
+        static const std::int64_t one = 1;
+        cv::Mat_<std::uint8_t> res(row, col);
+        for( int i = row -1; i >= 0; i --) {
+            for( int j = col - 1; j >=0 ; j --) {
+                res(i, j) = (n & one); 
+                n = n >> 1;
+            }
+        }
+        return res;
+    } 
 };
 
 }
--- unit_test/ChipImgProc/aruco_test.cpp
+++ unit_test/ChipImgProc/aruco_test.cpp
@@ -9,7 +9,28 @@ TEST(aruco, bin_to_int) {
     nlohmann::json mk_layout;
     fin >> mk_layout;
     for( auto&& mk : mk_layout["marker_list"] ) {
-        // std::cout << mk << std::endl;
         std::cout << chipimgproc::ArUco::bin_to_dec(mk) << std::endl;
     }
+    // int i = 0;
+    // int j = 0;
+    // for( auto&& mk : mk_layout["marker_list"] ) {
+    //     std::stringstream num; 
+    //     num << "\"" << chipimgproc::ArUco::bin_to_dec(mk) << "\"";
+
+
+    //     std::cout 
+    //         << std::setw(15) << num.str() 
+    //         << ": " 
+    //         << "[" << std::setw(2) << i << ", " << std::setw(2) << j << "],"
+    //         << std::endl;
+    //     j ++;
+    //     if( j == 7 ) {
+    //         i ++;
+    //         j = 0;
+    //     }
+    //     if( i == 7 ) break;
+    // }
+}
+TEST(aruco, int_to_mat ) {
+    std::cout << chipimgproc::ArUco::dec_to_mat(8117912230, 6, 6) << std::endl;
 }
