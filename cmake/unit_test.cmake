f94fcb7d30e9cdc24af454b7f868b967b8be9513
Chia Hua Chang <johnidfet@gmail.com>
2019-01-17 14:17:42 +0800

    pure submodule solution

--- cmake/unit_test.cmake
+++ cmake/unit_test.cmake
@@ -4,18 +4,9 @@ if( BUILD_TESTS)
     set(lib_targets ${lib_targets} ${__lib_targets})
     set(exe_targets ${exe_targets} ${__exe_targets})
 
-    message(STATUS "CI_JOB_TOKEN: $ENV{CI_JOB_TOKEN}")
     include(ExternalProject)
-    # ExternalProject_Add(TestData
-    #     URL "http://gitlab.centrilliontech.com.tw:10080/centrillion/ChipImgProcTestData/repository/v0.0.1/archive.tar.gz?private_token=$ENV{CI_JOB_TOKEN}"
-    #     CMAKE_ARGS
-    #         -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}/unit_test
-    # )
     ExternalProject_Add(TestData
-        GIT_REPOSITORY 
-            "https://gitlab-ci-token:Zbx3JboF4u3kMAWSLjSS@gitlab.centrilliontech.com.tw:10022/centrillion/ChipImgProcTestData" 
-        GIT_TAG
-            "900c1caba9f4955eab112624154d6c1f9c6e239d"
+        URL ${CMAKE_SOURCE_DIR}/lib/ChipImgProcTestData
         CMAKE_ARGS
             -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}/unit_test
     )
