c13624e3258b0a6c2e88b36ee963be91e95a34ed
Chia Hua Chang <johnidfet@gmail.com>
2019-01-17 13:53:30 +0800

    try use private_token

--- cmake/unit_test.cmake
+++ cmake/unit_test.cmake
@@ -7,10 +7,7 @@ if( BUILD_TESTS)
     message(STATUS "CI_JOB_TOKEN: $ENV{CI_JOB_TOKEN}")
     include(ExternalProject)
     ExternalProject_Add(TestData
-        GIT_REPOSITORY 
-            "https://gitlab-ci-token:$ENV{CI_JOB_TOKEN}@gitlab.centrilliontech.com.tw:10022/centrillion/ChipImgProcTestData" 
-        GIT_TAG
-            "900c1caba9f4955eab112624154d6c1f9c6e239d"
+        URL "http://gitlab.centrilliontech.com.tw:10080/centrillion/ChipImgProcTestData/repository/v0.0.1/archive.tar.gz?private_token=$ENV{CI_JOB_TOKEN}"
         CMAKE_ARGS
             -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}/unit_test
     )
