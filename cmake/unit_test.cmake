58a14d77bd4fdc0ee0a41f0787b045c3f66cb88f
Chia Hua Chang <johnidfet@gmail.com>
2019-01-17 11:08:37 +0800

    test ci job token

--- cmake/unit_test.cmake
+++ cmake/unit_test.cmake
@@ -4,11 +4,11 @@ if( BUILD_TESTS)
     set(lib_targets ${lib_targets} ${__lib_targets})
     set(exe_targets ${exe_targets} ${__exe_targets})
 
-    message(STATUS )
+    message(STATUS "CI_JOB_TOKEN: $ENV{CI_JOB_TOKEN}")
     include(ExternalProject)
     ExternalProject_Add(TestData
         GIT_REPOSITORY 
-            "ssh://gitlab-ci-token:${CI_JOB_TOKEN}:@gitlab.centrilliontech.com.tw:10022/centrillion/ChipImgProcTestData.git" 
+            "ssh://gitlab-ci-token:$ENV{CI_JOB_TOKEN}:@gitlab.centrilliontech.com.tw:10022/centrillion/ChipImgProcTestData.git" 
         GIT_TAG
             "900c1caba9f4955eab112624154d6c1f9c6e239d"
         CMAKE_ARGS
