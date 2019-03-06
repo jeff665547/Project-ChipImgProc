2155f02c2276817fc75cbbb54d08fc2faff25569
ChiaHuaChang <johnidfet@gmail.com>
2019-03-06 15:29:50 +0800

    update version 0.2.4

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -1,2 +1,2 @@
-screw_add_version( 0 2 3 "${CMAKE_SOURCE_DIR}/include/ChipImgProc/version.h")
+screw_add_version( 0 2 4 "${CMAKE_SOURCE_DIR}/include/ChipImgProc/version.h")
 message(STATUS "${PROJECT_NAME} v${${PROJECT_NAME}_VERSION}")
