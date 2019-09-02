01ff3b480e7011923609eedb361b4b7c9d8fe4bc
Chia Hua Chang <johnidfet@gmail.com>
2019-09-02 10:36:48 +0800

    update document

--- include/ChipImgProc/marker/detection/reg_mat.hpp
+++ include/ChipImgProc/marker/detection/reg_mat.hpp
@@ -1,3 +1,8 @@
+/**
+ *  @file   ChipImgProc/marker/detection/reg_mat.hpp
+ *  @author Chia-Hua Chang <johnidfet@centrilliontech.com.tw>
+ *  @brief  Detect markers in image and assume the marker layout is regular matrix distribution.
+ */
 #pragma once
 #include <ChipImgProc/utils.h>
 #include <ChipImgProc/marker/layout.hpp>
@@ -9,7 +14,10 @@
 #include <ChipImgProc/algo/fixed_capacity_set.hpp>
 #include <ChipImgProc/utils/pos_comp_by_score.hpp>
 namespace chipimgproc{ namespace marker{ namespace detection{
-
+/**
+ *  @brief  Detect markers in image and assume the marker layout is regular matrix distribution.
+ * 
+ */
 struct RegMat {
     template<class T>
     auto generate_raw_marker_regions(
@@ -137,6 +145,25 @@ struct RegMat {
         return marker_regions;
 
     }
+    /**
+     * @brief Call operator of RegMat type, given marker layout and return marker regions.
+     * 
+     * @tparam T            (Deduced) Input matrix value type, for centrillion Summit image, 
+     *                      the white channel usually use std::uint8_t. 
+     *                      and probe channel use std::uint16_t.
+     * @param src           Input image.
+     * @param mk_layout     Marker layout of current process image, 
+     *                      for different chip spec should have differenct chip marker layout. 
+     * @param unit          Image unit level, can be MatUnit::PX (pixel level) or MatUnit::CELL (cell level).
+     * @param cand_mk_i     Use the i-th candidate marker in marker layout to match the image. 
+     *                      By default is 0, usually means the standard marker pattern.
+     * @param out           The debug log message output, can be any STL ostream, by default is null stream
+     * @param v_bin         The debug pre-process image output callback, the callback form is void(const cv::Mat&) type. 
+     *                      Current implementation is 8bit normalization image.
+     * @param v_search      
+     * @param v_marker 
+     * @return std::vector<MKRegion> 
+     */
     template<class T>
     std::vector<MKRegion> operator()(
         const cv::Mat_<T>&      src, 
