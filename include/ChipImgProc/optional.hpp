f237fff8c38b9b8454887ab7ffc205117b2b7d1f
Chia-Hua Chang <CHChang810716@gmail.com>
2019-09-11 03:45:54 +0800

    update config

--- include/ChipImgProc/optional.hpp
+++ include/ChipImgProc/optional.hpp
@@ -1,10 +1,11 @@
 #pragma once
-#ifdef _MSC_VER
-  #include <boost/optional.hpp>
-  namespace std { 
-    template<class T>
-    using optional = boost::optional<T>;
-  }
-#else
-  #include <optional>
-#endif
+#include <optional>
+// #ifdef _MSC_VER
+//   #include <boost/optional.hpp>
+//   namespace std { 
+//     template<class T>
+//     using optional = boost::optional<T>;
+//   }
+// #else
+//   #include <optional>
+// #endif
