5f7cb8d899058e7181c6af18325130452ddc09b2
Jeff <jeff665547@gmail.com>
2020-12-04 16:57:29 +0800

    Bugfix: Fix unit_test for estimate_bias.hpp

--- unit_test/ChipImgProc/marker/detection/estimate_bias_test.cpp
+++ unit_test/ChipImgProc/marker/detection/estimate_bias_test.cpp
@@ -69,7 +69,7 @@ TEST(estimate_bias_test, simultaion_shift) {
 
 
     auto [bias, score] = chipimgproc::marker::detection::estimate_bias(
-        green_img, green_templ, green_mask, aruco_mk_pos, 0.42558601126675694
+        green_img, green_templ, green_mask, aruco_mk_pos, 0.42558601126675694, cv::Size2d(0.0, 0.0), 2.3, true, 0.0
     );
     std::cout << bias << std::endl;
     EXPECT_LT(std::abs(bias.x + 50), 3);
