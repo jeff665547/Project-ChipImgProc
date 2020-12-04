97c04701d51122d66242e0a2047eb4bdc1d75d8c
Jeff <jeff665547@gmail.com>
2020-12-04 15:54:46 +0800

    Feature: estimate bias between BF and fluorescent images by using customized patch(cover) and regulation.

--- include/ChipImgProc/marker/detection/estimate_bias.hpp
+++ include/ChipImgProc/marker/detection/estimate_bias.hpp
@@ -23,7 +23,11 @@ public:
         cv::Mat         templ,
         cv::Mat         mask,
         const Hints&    hints,
-        double          angle
+        double          angle,
+        cv::Size2d      basic_cover_size,
+        cv::Size2d      high_P_cover_size,
+        bool            regulation,
+        cv::Size2d      regulation_cover_size
     ) const {
         assert(!hints.empty());
         cv::Mat_<std::uint8_t> image;
@@ -68,44 +72,46 @@ public:
         int x1 = std::ceil(_x1 + templ_center.x);
         int y1 = std::ceil(_y1 + templ_center.y);
 
-        // Original: Scan fluor marks in all possible position (region). (*)
-        // cv::Size patch_size(image.cols - x1 + x0, image.rows - y1 + y0);
+        cv::Size2d cover_size;
+        bool default_cover = false;
+        if(high_P_cover_size != cv::Size2d(0.0, 0.0)){
+            std::cout << "use input" << std::endl;
+            // Substitutional: Scan fluor marks only in the estimated position (region) with high likelihood (99.5% up). (*)
+            cover_size.width = std::max(high_P_cover_size.width, basic_cover_size.width);
+            cover_size.height = std::max(high_P_cover_size.height, basic_cover_size.height);
+        }else{
+            std::cout << "use default" << std::endl;
+            // Original: Scan fluor marks in all possible position (region).
+            cover_size = cv::Size2d(image.cols - x1 + x0, image.rows - y1 + y0);
+            default_cover = true;
+        }
 
-        // Substitutional: Scan fluor marks only in the estimated position (region) with high likelihood (99.75% up). (*)
-        cv::Size patch_size;
-        cv::Size2d basic_cover(20.2*7.74, 20.2*7.74);
-        double high_P_cover_extend_ratio = 2.3;
-        patch_size.width = std::max(high_P_cover_extend_ratio*w, basic_cover.width);
-        patch_size.height = std::max(high_P_cover_extend_ratio*h, basic_cover.height);
-        // std::cout << "patch width: " << patch_size.width << " , patch height: " << patch_size.height << " pixels (px)" << std::endl;
 
-        
-        cv::Point2d patch_center((patch_size.width - 1) / 2.0, (patch_size.height - 1) / 2.0);
-        cv::Mat scores(cv::Mat::zeros(patch_size, score_matrix.type()));
+        cv::Point2d cover_center((cover_size.width - 1) / 2.0, (cover_size.height - 1) / 2.0);
+        cv::Mat scores(cv::Mat::zeros(cover_size, score_matrix.type()));
 
         for(auto&& h : hints) {
-            // Original: Original patch center (for match_template score domain) (*)
-            // cv::Point2f center(
-            //    h.x - x0 - templ_center.x + patch_center.x, 
-            //    h.y - y0 - templ_center.y + patch_center.y
-            // );
+            cv::Point2f center;
+            if(default_cover){
+                // Original: Original cover center (for match_template score domain) (*)
+                center.x = h.x - x0 - templ_center.x + cover_center.x;
+                center.y = h.y - y0 - templ_center.y + cover_center.y;
+            }else{
+                // Substitutional: Substitutional cover center (for match_template score domain) (*)
+                center.x = h.x - templ_center.x;
+                center.y = h.y - templ_center.y;
+            }
+            cv::Mat tmp, tmp2;
 
-            // Substitutional: Substitutional patch center (for match_template score domain) (*)
-            cv::Point2f center(
-                h.x - templ_center.x, 
-                h.y - templ_center.y
-            );
-
-            cv::Mat tmp;
             cv::getRectSubPix(score_matrix,
-                patch_size, center, tmp);
+                cover_size, center, tmp);
             scores += tmp;
         }
         cv::Point max_score_p;
         cv::minMaxLoc(scores, nullptr, nullptr, nullptr, &max_score_p);
 
         // Sub-pixel position bias estimated by Gaussian function
-        auto patch = scores(cv::Rect(
+        auto cover = scores(cv::Rect(
             max_score_p.x - 1,
             max_score_p.y - 1,
             3, 3
@@ -113,9 +119,9 @@ public:
         std::vector<double> zx(3);
         std::vector<double> zy(3);
 
-        for(int i = 0; i < patch.rows; i ++) {
-            for(int j = 0; j < patch.cols; j ++) {
-                auto n = patch.at<float>(i, j);
+        for(int i = 0; i < cover.rows; i ++) {
+            for(int j = 0; j < cover.cols; j ++) {
+                auto n = cover.at<float>(i, j);
                 zx[j] += n;
                 zy[i] += n;
             }
@@ -130,14 +136,34 @@ public:
         double dy = (0.5 * (zy[0] - zy[2])) / (zy[2] - (2 * zy[1]) + zy[0]);
 
         // Final estimation for bias correction
-        // Original: Bias correction for original case (all possible position (region)). (*)
-        // cv::Point2d bias(0 + max_score_p.x + dx - x0, 0 + max_score_p.y + dy - y0);
-
-        // Substitutional: Bias correction for Substitutional case (estimated position (region (99.75% up))). (*)
-        cv::Point2d bias(max_score_p.x + dx - patch_center.x, max_score_p.y + dy - patch_center.y);
-
-        // No adjustment.
-        // cv::Point2d bias(0, 0);
+        cv::Point2d bias;
+        if(regulation){
+            // Substitutional: Bias correction testing for no adjustment case & high precision case (estimated position (region (98.8% up))).
+            cv::Point rel_max_score_p, abs_rel_max_score_p;
+            cv::Size2d regulation_cover_radius;
+            rel_max_score_p.x = max_score_p.x - cover_center.x;
+            rel_max_score_p.y = max_score_p.y - cover_center.y;
+            abs_rel_max_score_p.x = std::abs(rel_max_score_p.x);
+            abs_rel_max_score_p.y = std::abs(rel_max_score_p.y);
+            regulation_cover_radius.width = regulation_cover_size.width / 2.0;
+            regulation_cover_radius.height = regulation_cover_size.height / 2.0;
+            
+            if(abs_rel_max_score_p.x > regulation_cover_radius.width || abs_rel_max_score_p.y > regulation_cover_radius.height){
+                bias.x = 0.0;
+                bias.y = 0.0;
+            }else{
+                bias.x = rel_max_score_p.x + dx;
+                bias.y = rel_max_score_p.y + dy;
+            }
+        }else if(!default_cover){
+            // Substitutional: Bias correction for Substitutional case (estimated position (region (99.5% up))).
+            bias.x = max_score_p.x + dx - cover_center.x;
+            bias.y = max_score_p.y + dy - cover_center.y;
+        }else{
+            // Original: Bias correction for original case (all possible position (region)).
+            bias.x = 0 + max_score_p.x + dx - x0;
+            bias.y = 0 + max_score_p.y + dy - y0;
+        }
 
         return std::make_tuple(
             bias, 
@@ -149,7 +175,11 @@ public:
         cv::Mat         templ,
         cv::Mat         mask,
         Hints           hints_rum,
-        cv::Mat         warp_mat
+        cv::Mat         warp_mat,
+        cv::Size2d      basic_cover_size = cv::Size2d(0.0, 0.0),
+        double          high_P_cover_extend_r = 0.0,
+        bool            regulation = false,
+        double          regulation_cover_extend_r = 0.0
     ) const {
         for(auto& h : hints_rum) {
             h.x -= 0.5;
@@ -158,9 +188,26 @@ public:
         std::vector<cv::Point2d> hints_dst(hints_rum.size());
         cv::transform(hints_rum, hints_dst, warp_mat);
         auto angle = rotation::from_warp_mat(warp_mat);
+        
+        // Use different estimate cover and regulation for different scan mode.
+        cv::Size2d regulation_cover_size;
+        cv::Size2d high_P_cover_size;
+        
+        if(regulation){
+            regulation_cover_size.width = regulation_cover_extend_r * templ.cols;
+            regulation_cover_size.height = regulation_cover_extend_r * templ.rows;
+        }
+        
+        high_P_cover_size.width = high_P_cover_extend_r * templ.cols;
+        high_P_cover_size.height = high_P_cover_extend_r * templ.rows;
+
         return this->operator()(
             _image, templ, mask,
-            hints_dst, -angle
+            hints_dst, -angle,
+            basic_cover_size, 
+            high_P_cover_size,
+            regulation,
+            regulation_cover_size
         );
     }
 };
